d3f7480c95d120c03fe508cabaeaf19fc66cc014
Johnny Vanhentenryk <johnny.vanhentenryk@bnpparibasfortis.com>
2021-02-25 17:06:49 +0100

    Working react app with RBAC on auth0

---
+++ src/posts.js
@@ -0,0 +1,24 @@
+const posts = [
+	{
+	  id: "a",
+	  title: "Post a",
+	  ownerId: "auth0|603684953e57540070774bbc"
+	},
+	{
+	  id: "b",
+	  title: "Post b",
+	  ownerId: "other user id"
+	},
+	{
+	  id: "c",
+	  title: "Post c",
+	  ownerId: "auth0|603684953e57540070774bbc"
+	},
+	{
+	  id: "d",
+	  title: "Post d",
+	  ownerId: "other user id 2"
+	}
+  ];
+  
+  export default posts;
